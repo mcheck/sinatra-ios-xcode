99d76f77dcdc6601d855228c9a222d1d0410185a
mcheck <mcheck@sologroup.com>
2012-08-02 14:59:41 -0500

    Initial Commit

---
+++ sinatra-ios/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  sinatra-ios
+//
+//  Created by Michael Check on 8/2/12.
+//  Copyright (c) 2012 Solo Group, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
